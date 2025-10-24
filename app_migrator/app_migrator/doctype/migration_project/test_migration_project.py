a166edcb8ffac91ad05c52270f3467af42503c7c
rogerboy38 <rogerboy38@gmail.com>
2025-10-24 08:24:46 -0600

    fix: remove duplicate nested app_migrator structure
    - Remove app_migrator/app_migrator/ duplicate folder
    - Keep only correct V7.0.1 enterprise structure
    - Fix Frappe app installation compatibility

--- app_migrator/app_migrator/doctype/migration_project/test_migration_project.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Roger Boy and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestMigrationProject(FrappeTestCase):
-	pass
